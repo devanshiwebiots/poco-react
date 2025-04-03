a8dde3a52b25287253be4437cb7b5b144765f9de
tusharwebiots <162977690+tusharwebiots@users.noreply.github.com>
2025-04-03 14:47:17 +0530

    Update footer.js

--- src/component/common/footer/footer.js
+++ src/component/common/footer/footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => {
       <Container fluid={true}>
         <Row>
           <Col md="6" className="footer-copyright">
-            <p className="mb-0">Copyright © 2023 Poco. All rights reserved.</p>
+            <p className="mb-0">Copyright © 2025 Poco. All rights reserved.</p>
           </Col>
           <Col md="6">
             <p className="pull-right mb-0">
